90f5a7eab3b5a864869b2121e90baa975e3619d2
diogopaza <diogopazacvel@gmail.com>
2019-08-29 16:40:06 -0300

    grpc

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net"
+	pb "servergrpc/proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+//type server struct{}
+
+func main() {
+	listener, err := net.Listen("tcp", ":4040")
+	if err != nil {
+		panic(err)
+	}
+
+	srv := grpc.NewServer()
+	//pb.RegisterAddServiceServer(srv, &server{})
+	pb.RegisterAddServicePessoaServer(srv, &pb.Server{})
+	reflection.Register(srv)
+
+	if e := srv.Serve(listener); e != nil {
+		panic(e)
+	}
+
+}
